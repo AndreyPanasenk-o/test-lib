15eaf4af195eedfa4855e3327feb0fea7c68dc02
designpand <pand@isddesign.com>
2018-10-26 18:01:00 +0300

    add failed test

--- packages/nec-core/src/app/app.component.spec.ts
+++ packages/nec-core/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to nec-core!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to nec-core!2');
   });
 });
