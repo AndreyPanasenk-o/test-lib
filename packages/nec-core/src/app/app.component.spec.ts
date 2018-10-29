313f0e1e7a0271624db2dad546d20e582f2b5885
designpand <pand@isddesign.com>
2018-10-29 13:34:01 +0200

    change titles

--- packages/nec-core/src/app/app.component.spec.ts
+++ packages/nec-core/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'nec-core'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('nec-core');
+    expect(app.title).toEqual('nec-core1');
   });
 
   it('should render title in a h1 tag', () => {
