c508459fcd9c1019ada22895d797fdc2c63fa9ee
designpand <pand@isddesign.com>
2018-10-29 10:13:23 +0200

    add checkExitCode

--- Build/BuildHelper.ps1
+++ Build/BuildHelper.ps1
@@ -14,6 +14,7 @@ function build{
 function test{
     Push-Location $PSScriptRoot\..\packages\nec-core
     ng test
+    checkExitCode
     Pop-Location
 }
 
@@ -21,6 +22,21 @@ function publish{
     Push-Location $PSScriptRoot\..\packages\nec-core\dist\nec-core-lib
     npm publish --registry $npmRegistry
     Pop-Location
+}
+
+function checkExitCode{
+    echo "Exit code: $lastExitCode"
+    checkExitCodeCore $lastExitCode
+}
+function checkExitCodeCore{
+    [CmdletBinding()]
+    param(
+        [int]$exitCode
+    )
+    echo $exitCode
+    if($exitCode -gt 0){
+        exit $exitCode
+    }
 }
 if($operation -eq 'build'){ build }
 if($operation -eq 'test'){ test }
